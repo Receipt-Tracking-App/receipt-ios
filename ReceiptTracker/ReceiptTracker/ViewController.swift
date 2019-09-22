4fe4165aecae162a09135fdf3a6f0278ad455db3
Jordan Christensen <jordan.c4922@gmail.com>
2019-09-23 05:35:43 +0800

    Add xcode project

---
+++ ReceiptTracker/ReceiptTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReceiptTracker
+//
+//  Created by Jordan Christensen on 9/23/19.
+//  Copyright © 2019 Mazjap Co Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
