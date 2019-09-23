363c0c4f3dcfea0ffb783216d13a51c40bd176cc
Donovan Langager <donovan.lan16@gmail.com>
2019-09-23 12:22:01 -0700

    Added view controllers and files

---
+++ ReceiptTracker/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  ReceiptTracker
+//
+//  Created by Uptiie on 9/23/19.
+//  Copyright © 2019 Mazjap Co Technologies. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
