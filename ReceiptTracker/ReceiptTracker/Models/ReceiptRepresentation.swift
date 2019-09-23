cc7ac3829cc0b668dba87d80bc93c6b6b7a90e86
Jordan Christensen <jordan.c4922@gmail.com>
2019-09-24 04:37:36 +0800

    Add User and Receipt objects

--- ReceiptTracker/ReceiptTracker/Models/ReceiptRepresentation.swift
+++ ReceiptTracker/ReceiptTracker/Models/ReceiptRepresentation.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import CoreData
+
+enum CodingKeys: String, CodingKey {
+    case id
+    case purchaseDate = "purchase_date"
+    case merchant
+    case amount
+    case notes
+    case createdAt = "created_at"
+    case updatedAt = "updated_at"
+    case userID = "user_id"
+}
+
+struct ReceiptRepresentation: Codable, Equatable {
+    var id: Int64
+    var purchaseDate: Date
+    var merchant: String
+    var amount: Double
+    var notes: String
+    var createdAt: Date
+    var updatedAt: Date
+    var userID: Int64
+}
