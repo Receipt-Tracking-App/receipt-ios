78e2e6e21e5cc3cc69aa1247f622ce8798c55de6
Donovan Langager <donovan.lan16@gmail.com>
2019-09-24 10:36:32 -0700

    Added Tableview Controller

---
+++ ReceiptTracker/ReceiptTracker/View Controllers/ReceiptTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReceiptTableViewCell.swift
+//  ReceiptTracker
+//
+//  Created by Uptiie on 9/23/19.
+//  Copyright © 2019 Mazjap Co Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ReceiptTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
